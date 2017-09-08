d7f93cb2319a7fe0d4ce2a08674bd9471e9cdcfc
David Prause <praused@hotmail.com>
2017-09-08 11:08:32 -0400

    Add project files.

---
+++ Ch12Ex04_GenericClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12Ex04_GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
