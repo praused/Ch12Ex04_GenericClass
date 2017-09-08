f38821b79bba682d2a2770b67c0d0033db855876
David Prause <praused@hotmail.com>
2017-09-08 11:15:37 -0400

    Add existing classes from another project

---
+++ Ch12Ex04_GenericClass/Chicken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12Ex04_GenericClass
+{
+    public class Chicken : Animal
+    {
+        public void LayEgg()
+        {
+            Console.WriteLine($"{name} has laid an egg.");
+        }
+
+        public Chicken(string newName) : base(newName)
+        {
+        }
+    }
+}
---
+++ Ch12Ex04_GenericClass/Cow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12Ex04_GenericClass
+{
+    public class Cow : Animal
+    {
+        public void Milk()
+        {
+            Console.WriteLine($"{name} has been milked.");
+        }
+
+        public Cow(string newName) : base(newName)
+        {
+        }
+    }
+}
